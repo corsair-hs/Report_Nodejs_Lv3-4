c55d8d829919c90a474ee72b85895d0589770939
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-27 02:32:28 +0900

    migrations 정의

--- migrations/20230426144716-create-comments.js
+++ migrations/20230426144716-create-comments.js
@@ -2,8 +2,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Posts', {
-      postId: {
+    await queryInterface.createTable('Comments', {
+      commentId: {
         allowNull: false, // NOT NULL
         autoIncrement: true, // AUTO_INCREMENT
         primaryKey: true, // Primary Key (기본키)
@@ -13,16 +13,21 @@ module.exports = {
         allowNull: false, // NOT NULL
         type: Sequelize.INTEGER,
         references: {
-          model: 'Users', // Users 테이블을 참조합니다.
-          key: 'userId', // Users 테이블의 userId 컬럼을 참조합니다.
+          model: 'Users', // Users 모델을 참조합니다.
+          key: 'userId', // Users 모델의 userId를 참조합니다.
         },
-        onDelete: 'CASCADE', // 만약 Users 테이블의 userId 값이 삭제되면, Posts 테이블의 데이터가 삭제됩니다.
+        onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Comments 모델의 데이터가 삭제됩니다.
       },
-      title: {
+      PostId: {
         allowNull: false, // NOT NULL
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Posts', // Posts 모델을 참조합니다.
+          key: 'postId', // Posts 모델의 postId를 참조합니다.
+        },
+        onDelete: 'CASCADE', // 만약 Posts 모델의 postId가 삭제되면, Comments 모델의 데이터가 삭제됩니다.
       },
-      content: {
+      comment: {
         allowNull: false, // NOT NULL
         type: Sequelize.STRING
       },
@@ -39,6 +44,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Posts');
+    await queryInterface.dropTable('Comments');
   }
 };
